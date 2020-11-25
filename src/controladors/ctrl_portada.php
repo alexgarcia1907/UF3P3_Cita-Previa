5126262eaaabc671cb2be68c152b419d4f0850d9
Alex <agp.2000@hotmail.com>
2020-11-25 16:31:50 +0100

    ctrl portada

---
+++ src/controladors/ctrl_portada.php
@@ -0,0 +1,14 @@
+<?php
+
+
+function ctrl_portada($sesio,$usuario,$cita){
+
+    if ( !$sesio -> sesiousuari()) {
+        header("Location: index.php?r=login");
+        die();
+      }
+
+    $citesusu = $cita -> getdades($usuario -> getid($sesio -> obtenirnom()));
+
+    
+}
