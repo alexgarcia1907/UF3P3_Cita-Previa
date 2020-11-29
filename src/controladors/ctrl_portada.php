abbd183acf04286cfa0726b077c243e73d956382
Sikuu <vguitart6@gmail.com>
2020-11-29 13:55:48 +0100

    login funcionant

--- src/controladors/ctrl_portada.php
+++ src/controladors/ctrl_portada.php
@@ -2,10 +2,10 @@
 
 function ctrl_portada($sesio,$usuario,$cita){
 
-     /* if ( !$sesio -> sesiousuari()) {
+      if ( !$sesio -> sesiousuari()) {
         header("Location: index.php?r=login");
         die();
-      }*/
+      }
 
     $citesusu = $cita -> getdades($usuario -> getid("test"));
     print_r($citesusu);
